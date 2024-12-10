5a7afc8e70db4403da74622da2ede63521b31b11
Comfort Ngwenya <>
2024-12-10 09:34:26 +0200

    Addingt the create restaurant admin route

--- restaurent-backend/routes/authRoutes.js
+++ restaurent-backend/routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { login} = require('../controllers/authController');
+const { login, createRestaurantAdmin } = require('../controllers/authController');
+const { protect } = require('../middlewares/authMiddleware.js');
 
 const router = express.Router();
 
+
+router.post('/create-restaurant-admin', protect(['super_admin']), createRestaurantAdmin);
 
 router.post('/login', login);
 
