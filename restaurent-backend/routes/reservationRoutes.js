ee44e90d5a4699e17128330c11a045e16b1c4e95
Comfort Ngwenya <>
2024-12-27 10:43:57 +0200

    Updating reservations routes

--- restaurent-backend/routes/reservationRoutes.js
+++ restaurent-backend/routes/reservationRoutes.js
@@ -1,21 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const { protect } = require('../middlewares/authMiddleware.js');
-// const reservationController = require('../controllers/reservationController.js');
-
-// // User routes
-// router.post('/', protect(['user']), reservationController.createReservation);
-// router.get('/user', protect(['user']), reservationController.getUserReservations);
-// router.put('/:reservationId', protect(['user']), reservationController.updateUserReservation);
-// router.delete('/:reservationId', protect(['user']), reservationController.cancelUserReservation);
-
-// // Admin routes
-// router.get('/restaurant/:restaurantId', protect(['restaurant_admin']), reservationController.getRestaurantReservations);
-// router.put('/status/:reservationId', protect(['restaurant_admin']), reservationController.updateReservationStatus);
-
-// module.exports = router;
-
-
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middlewares/authMiddleware.js');
@@ -35,5 +17,7 @@ router.delete('/:reservationId', protect(['user']), reservationController.cancel
 router.post('/', protect(['restaurant_admin']), reservationController.createReservation);
 router.get('/restaurant/:restaurantId', protect(['restaurant_admin']), reservationController.getRestaurantReservations);
 router.put('/status/:reservationId', protect(['restaurant_admin']), reservationController.updateReservationStatus);
+router.get('/availability/:restaurantId/:date', reservationController.getAvailabilityByDate);
+router.post('/create-reservation', protect(['restaurant_admin']), reservationController.createReservation);
 
 module.exports = router;
